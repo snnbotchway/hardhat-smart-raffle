fb6c9f8f49476e096dcc937e551ce209ad408a2a
snnbotchway <solomonbotchway7@gmail.com>
2023-05-06 19:40:09 +0000

    Add structure from hardhat starter kit

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,30 +10,77 @@ import "@nomicfoundation/hardhat-toolbox"
 import "@nomiclabs/hardhat-etherscan"
 import "@nomiclabs/hardhat-solhint"
 
-const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || ""
-const PRIVATE_KEY =
-    process.env.PRIVATE_KEY || "1111111111111111111111111111111111111111111111111111111111111111"
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
-const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+const MAINNET_RPC_URL =
+    process.env.MAINNET_RPC_URL ||
+    process.env.ALCHEMY_MAINNET_RPC_URL ||
+    "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
+const POLYGON_MAINNET_RPC_URL =
+    process.env.POLYGON_MAINNET_RPC_URL || "https://polygon-mainnet.alchemyapi.io/v2/your-api-key"
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+// optional
+const MNEMONIC = process.env.MNEMONIC || "Your mnemonic"
+const FORKING_BLOCK_NUMBER = process.env.FORKING_BLOCK_NUMBER
+
+// Your API key for Etherscan, obtain one at https://etherscan.io/
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "Your etherscan API key"
+const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY || "Your polygonscan API key"
 
 const config: HardhatUserConfig = {
-    solidity: "0.8.18",
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.18",
+            },
+        ],
+    },
+    defaultNetwork: "hardhat",
     networks: {
+        hardhat: {
+            hardfork: "merge",
+            // If you want to do some forking set `enabled` to true
+            forking: {
+                url: MAINNET_RPC_URL,
+                blockNumber: Number(FORKING_BLOCK_NUMBER),
+                enabled: false,
+            },
+            chainId: 31337,
+        },
         localhost: {
-            url: "http://localhost:8545",
             chainId: 31337,
         },
         sepolia: {
-            url: SEPOLIA_RPC_URL,
-            accounts: [PRIVATE_KEY],
+            url: SEPOLIA_RPC_URL !== undefined ? SEPOLIA_RPC_URL : "",
+            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+            //   accounts: {
+            //     mnemonic: MNEMONIC,
+            //   },
+            saveDeployments: true,
             chainId: 11155111,
+        },
+        mainnet: {
+            url: MAINNET_RPC_URL,
+            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+            //   accounts: {
+            //     mnemonic: MNEMONIC,
+            //   },
+            saveDeployments: true,
+            chainId: 1,
+        },
+        polygon: {
+            url: POLYGON_MAINNET_RPC_URL,
+            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+            saveDeployments: true,
+            chainId: 137,
         },
     },
     etherscan: {
-        apiKey: ETHERSCAN_API_KEY,
+        sepolia: ETHERSCAN_API_KEY,
+        mainnet: ETHERSCAN_API_KEY,
+        polygon: POLYGONSCAN_API_KEY,
     },
     gasReporter: {
-        enabled: true,
+        enabled: process.env.REPORT_GAS !== undefined,
         outputFile: "gasReport.txt",
         noColors: true,
         currency: "GHS",
@@ -44,6 +91,13 @@ const config: HardhatUserConfig = {
         deployer: {
             default: 0,
         },
+    },
+    mocha: {
+        timeout: 200000, // 200 seconds max for running tests
+    },
+    typechain: {
+        outDir: "typechain",
+        target: "ethers-v5",
     },
 }
 
